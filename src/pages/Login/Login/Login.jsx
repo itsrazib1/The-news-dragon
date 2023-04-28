2cbd04a6aa8598a7519fcb25e1ee68e1caaa02c4
itsrazib1 <itsrazib1@gmail.com>
2023-04-28 22:58:59 +0600

    first commit

---
+++ src/pages/Login/Login/Login.jsx
@@ -0,0 +1,64 @@
+import React, { useContext } from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../../providers/AuthProvider';
+
+
+const Login = () => {
+    const {singIn} = useContext(AuthContext);
+    const navigate = useNavigate();
+const location = useLocation();
+console.log("login page location ",location);
+const From = location.state?.from?.pathname || '/category/0'
+    const handellogin = event => {
+        event.preventDefault();
+        const from =  event.target ;
+        const email = from.email.value;
+        const password = from.password.value;
+        singIn(email,password)
+        .then(result => {
+            const loggedUser = result.user;
+            console.log(loggedUser);
+            
+            navigate(From ,{replace: true})
+        })
+        .catch(error => {
+            console.log(error);
+        })
+
+    } 
+
+    return (
+        <Container className='w-25 mx-auto'>
+            <h3>Please Login</h3>
+            <Form onSubmit={handellogin}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name='email' placeholder="Enter email" required />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name='password' placeholder="Password" required />
+                </Form.Group>
+
+                
+                <Button variant="primary" type="submit">
+                    Login
+                </Button>
+                <br />
+                <Form.Text className="text-secondary">
+                    Don't Have an Account? <Link to="/register">Register</Link>
+                </Form.Text>
+                <Form.Text className="text-success">
+
+                </Form.Text>
+                <Form.Text className="text-danger">
+
+                </Form.Text>
+            </Form>
+        </Container>
+    );
+};
+
+export default Login;
